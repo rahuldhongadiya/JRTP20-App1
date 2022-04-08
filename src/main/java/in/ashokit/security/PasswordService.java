ca4a6b153b7846bce1092259262d0b6027e0d271
rahuldhongadiya <rahuldhongadiya1996@gmail.com>
2022-04-08 11:07:54 +0530

    long l

--- src/main/java/in/ashokit/security/PasswordService.java
+++ src/main/java/in/ashokit/security/PasswordService.java
@@ -5,7 +5,7 @@ import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 public class PasswordService {
-
+    long l = 7674649l;
 	public static String encode(String txt) {
 		Encoder encoder = Base64.getEncoder();
 		return encoder.encodeToString(txt.getBytes());
